835c21755f18c19ed3aff34e0b61ea1a2b992e5b
Tenclar <tenclarvalus@gmail.com>
2019-10-24 10:53:36 -0500

    Rotas

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
