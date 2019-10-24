0812517236e017c2cf7647c19d7ba087b8bf8fbc
Tenclar <tenclarvalus@gmail.com>
2019-10-24 16:37:59 -0500

    Index and store appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,16 +1,21 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
@@ -36,7 +41,7 @@ class AppointmentController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'validation fails' });
+      return res.status(400).json({ error: 'validation fields fails' });
     }
     const { provider_id, date } = req.body;
 
@@ -44,15 +49,19 @@ class AppointmentController {
      * Check if provider_id is a providees
      */
 
-    const checkisProvider = await User.findOne({
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true }
     });
 
-    if (!checkisProvider) {
+    if (!checkIsProvider) {
       return res
         .status(401)
         .json({ error: 'you can only create appointment with providers' });
     }
+    /**
+     * Check for past dates
+     */
+
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
@@ -71,12 +80,31 @@ class AppointmentController {
         .status(400)
         .json({ error: 'Appointment date is not available' });
     }
+    /**
+     * Create Appointments
+     *
+     */
 
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date: hourStart
     });
+    /**
+     *Notify appointment Provider
+     *
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às ' H:mm'h'",
+      { locale: ptBR }
+    );
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id
+    });
+
     return res.json(appointment);
   }
 }
