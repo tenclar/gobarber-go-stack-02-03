eb77814b8a4dbe1be8b6f7c6c71e6964aed874bf
Tenclar <tenclarvalus@gmail.com>
2019-10-26 09:33:55 -0500

    Configuração de Variáveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '534045f044263c',
-    pass: '945dd621a1cd37'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'Equipe GoBarber < noreply@gobarber.com'
