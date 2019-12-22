1192527993c6b78cccb7a02cb53c71c67773d3b2
tenclar <tenclarvalus@gmail.com>
2019-12-22 16:05:04 -0500

    configuração cors na api

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import * as Sentry from '@sentry/node';
 import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
@@ -22,6 +23,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
