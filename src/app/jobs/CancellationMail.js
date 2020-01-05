38df102bbc2085ad61a78b36e2da9338519f90d2
Tenclar <tenclarvalus@gmail.com>
2020-01-04 21:28:24 -0500

    formatando envio de email

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -1,6 +1,6 @@
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import Mail from '../../lib/mail';
+import Mail from '../../lib/Mail';
 
 class CancellationMail {
   get key() {
@@ -9,6 +9,13 @@ class CancellationMail {
 
   async handle({ data }) {
     const { appointment } = data;
+    const dta = format(
+      parseISO(appointment.date),
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {
+        locale: ptBR
+      }
+    );
 
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
@@ -17,13 +24,7 @@ class CancellationMail {
       context: {
         provider: appointment.provider.name,
         user: appointment.user.name,
-        date: format(
-          parseISO(appointment.date),
-          "'dia' dd 'de' MMMM', às' H:mm'h'",
-          {
-            locale: ptBR
-          }
-        )
+        date: dta
       }
     });
   }
